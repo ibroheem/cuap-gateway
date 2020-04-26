114b2186ab19d2cb55b4c71f8416ab7a175b262b
ibroheem <movichtech@gmail.com>
2020-04-26 17:34:37 +0100

    Added pdu/session_phone.h

---
+++ pdu/session_phone.h
@@ -0,0 +1,11 @@
+
+#include "session_sa.h"
+
+///! Session initiated by phone resembles that initiated by SA
+
+///! Session init by Phone will also use the types defined by session_sa.h
+///! Thus, this header file is just a stub
+
+namespace cuap::pdu
+{
+}
